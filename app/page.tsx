b9a3a14284c3acfc662e12e9473949adf3989a55
Moiz Qureshi <m.moiz782008@gmail.com>
2025-01-09 15:04:25 +0500

    commint
    push

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import BmiCalculator from '@/components/bmi-calculator'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <BmiCalculator/>
+    </>
+  )
+}
